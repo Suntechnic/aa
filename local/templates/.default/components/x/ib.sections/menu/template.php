1f56ab3484c239da39a40c798a18bcb2d2664ffb
Александр Маджугин <madzhugin@gmail.com>
2026-06-01 14:40:19 +0000

    изменения по верстке

--- local/templates/.default/components/x/ib.sections/menu/template.php
+++ local/templates/.default/components/x/ib.sections/menu/template.php
@@ -4,14 +4,18 @@
  * @var array $arParams
  */
 ?>
-<nav class="menu__body js-nav" data-da=".menu__pages, 992, 2">
-    <ul class="menu__list">
-        <?foreach($arResult['ITEMS'] as $dctItem):?>
-        <li class="menu__item <?if($arParams['TEMPLATE_VARS']['SECTION_CODE'] == $dctItem['CODE']):?>_item-active<?endif;?>">
-            <a href="<?=$dctItem['SECTION_PAGE_URL']?>" class="menu__link text-16" >
-                <?=$dctItem['NAME']?>
-            </a>
-        </li>
-        <?endforeach;?>
-    </ul>
-</nav>
+<div class="catalog__container">
+
+    <nav class="menu__body js-nav" data-da=".menu__pages, 992, 2">
+        <ul class="menu__list">
+            <?foreach($arResult['ITEMS'] as $dctItem):?>
+            <li class="menu__item <?if($arParams['TEMPLATE_VARS']['SECTION_CODE'] == $dctItem['CODE']):?>_item-active<?endif;?>">
+                <a href="<?=$dctItem['SECTION_PAGE_URL']?>" class="menu__link text-16" >
+                    <?=$dctItem['NAME']?>
+                </a>
+            </li>
+            <?endforeach;?>
+        </ul>
+    </nav>
+
+</div>
